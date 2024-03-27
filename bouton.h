63f70e928237481e9e7bbb396c3afa457a3b7a18
Sluggcat <157683539+Sluggcat@users.noreply.github.com>
2024-03-27 16:55:44 +0100

    update
    filling

---
+++ bouton.h
@@ -0,0 +1,13 @@
+#ifndef BOUTON_H
+#define BOUTON_H
+
+class Bouton
+{
+    public :
+    void initialiser();
+    void charge();
+    int stop();
+
+};
+
+#endif
--- lecteurcarte.h
+++ lecteurcarte.h
@@ -1,13 +1,21 @@
 #ifndef LECTEURCARTE_H
 #define LECTEURCARTE_H
-#include <lcarte.h>
-
+//#include <lcarte.h>
 
 class LecteurCarte
 {
-  public : 
-	void initialiser();
-	void lire_carte();
+	private :
+		int id ;
+
+  	public :
+		int id_vehicule ; 
+
+		void initialiser();
+		void lire_carte();
+		int get_id();
+		void attente_insertion();
+		void attente_retrait();
+
 };
 
 #endif // LECTEURCARTE_H
